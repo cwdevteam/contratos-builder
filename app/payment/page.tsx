fe5af5de27823562144e91bbbd42b9a0234b309f
Nick Carney <npcarney@uvm.edu>
2024-10-26 18:03:05 -0400

    fixing payment page for deployment

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,45 +1,43 @@
-'use client'
+'use client';
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import useDynamicPageStore from '../store/use[page]';
 
-
 const Payment = () => {
+  const query = useSearchParams();
+  const paid = query.get('success');
+  const pages = useDynamicPageStore((state) => state.pages);
+  const emails = [''];
+  const [message, setMessage] = useState('');
 
-    const query = useSearchParams();
-    const paid = query.get('success');
-    const pages = useDynamicPageStore((state) => state.pages);
-    const emails = [''];
+  // Collect email addresses from pages
+  Object.keys(pages).forEach((id) => {
+    const pageData = pages[Number(id)];
+    emails.push(pageData.email);
+  });
 
-    Object.keys(pages).forEach((id) => {
-        const pageData = pages[Number(id)];
-        emails.push(pageData.email);
-        });
-
-        useEffect(() => {
-            const successElement = document.getElementById('success');
-            if (successElement) {
-              if (paid === 'true') {
-                successElement.innerHTML = `A contract will be sent to ${emails}`;
-              } else {
-                successElement.innerHTML = `Your payment was not successful`;
-              }
-            }
-          }, [paid, emails]);
-    
+  // Set message based on payment status
+  useEffect(() => {
+    if (paid === 'true') {
+      setMessage(`A contract will be sent to ${emails}`);
+    } else {
+      setMessage('Your payment was not successful');
+    }
+  }, [paid, emails]);
 
   return (
     <div className="min-h-screen p-4 sm:p-8 flex flex-col justify-between">
-        <main>
+      <main>
         <div className="w-full sm:w-1/2 p-4 sm:p-8 flex flex-col justify-center">
           <p className="text-lg sm:text-xl mb-8">Congrats! You&apos;re protecting your art.</p>
           <div className="text-base sm:text-lg" id="success">
+            {message}
           </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
