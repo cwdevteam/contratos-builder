02fc1a04964a89f20feebb426b64e84cde2153c2
Nick Carney <npcarney@uvm.edu>
2024-10-27 13:01:55 -0400

    editing api version

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16', // Use the latest API version
+  apiVersion: '2023-8-16', // Use the latest API version
 });
 
 export async function POST() {
