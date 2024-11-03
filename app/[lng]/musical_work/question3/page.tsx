47b169f1ee0c08fec8dd722b3cea05f63feb4281
Nick Carney <npcarney@uvm.edu>
2024-11-02 23:58:40 -0400

    musical_work/question3 translation

--- app/[lng]/musical_work/question3/page.tsx
+++ app/[lng]/musical_work/question3/page.tsx
@@ -4,8 +4,13 @@ import React, { Suspense, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useSearchParams } from 'next/navigation'
 import useQuestion3 from '../../store/useQuestion3'
+import { useTranslation } from '@/app/i18n/client'
 
-const ContractBuilder3 = () => {
+const ContractBuilder3 = ({ params }: {
+  params: {
+    lng: string;
+  };
+}) => {
   const { push } = useRouter()
   const updateContributorCount = useQuestion3((state) => state.updateContributorCount);
   const [pageCount, setPageCount] = useState<number | null>(null)
@@ -29,6 +34,9 @@ const ContractBuilder3 = () => {
 
   }
 
+  let {lng} = params
+  const { t } = useTranslation(lng, 'musical_work/question3')
+
   return (
     <div className="min-h-screen p-4 sm:p-8 flex flex-col justify-between">
       <main className="flex flex-col sm:flex-row gap-6 sm:gap-8">
@@ -37,15 +45,15 @@ const ContractBuilder3 = () => {
             onClick={() => push('/question1')}
             className="text-xs sm:text-sm text-gray-500 w-full text-left mb-2 border-none"
           >
-            What type of splits contract would you like to create?
+            {t('back1')}
           </button>
           <button
             onClick={() => push('/musical_work/question2')}
             className="text-xs sm:text-sm text-gray-500 w-full text-left mb-2 border-none"
           >
-            What is the name of the song?
+            {t('back2')}
           </button>
-          <p>How many collaborators contributed to writing the song?</p>
+          <p>{t('p1')}</p>
           <form className="flex flex-col">
             <input
               type="number"
@@ -59,30 +67,22 @@ const ContractBuilder3 = () => {
         </div>
         <div className=" p-8 py-1">
           <p className="text-xs text-gray-500">
-            Your contract has yet to be completed. Continue to fill out the
-            decision tree.
+            {t('p2')}
           </p>
-          <h3>1.0 Music Work Identification</h3>
+          <h3>{t('h3')}</h3>
           <p>
-            The contracting parties have collaborated in the authorship and
-            composition of the musical work titled{' '}
+            {t('p3')}{' '}
             <span className="text-red-500">{song}</span>
           </p>
         </div>
       </main>
       <footer className="flex flex-col gap-6 row-start-3">
         <button onClick={handleSubmit} className="border border-red">
-          SUBMIT
+        {t('submit')}
         </button>
       </footer>
     </div>
   )
 }
 
-const WrappedContractBuilder3 = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <ContractBuilder3 />
-  </Suspense>
-)
-
-export default WrappedContractBuilder3
+export default ContractBuilder3
