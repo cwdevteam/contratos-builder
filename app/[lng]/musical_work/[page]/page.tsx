632c84ba87ac36b17e3fb32232a2e6d3aeac68b3
Nick Carney <npcarney@uvm.edu>
2024-11-03 23:38:55 -0500

    tranlation for dynamic contributor info page

--- app/[lng]/musical_work/[page]/page.tsx
+++ app/[lng]/musical_work/[page]/page.tsx
@@ -3,12 +3,17 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter, useSearchParams, useParams } from 'next/navigation'
 import useDynamicPageStore from '../../store/use[page]'
+import { useTranslation } from '@/app/i18n/client'
 
-const DynamicPage = () => {
+const DynamicPage = ({ params }: {
+  params: {
+    lng: string;
+  };
+}) => {
   const router = useRouter()
-  const params = useParams()
+  const useParams1 = useParams()
   const searchParams = useSearchParams()
-  const pageNumber = Number(params.page)
+  const pageNumber = Number(useParams1.page)
   const pageCount = Number(searchParams.get('pageCount'))
   const lastSplit = Number(searchParams.get('split'))
 
@@ -21,8 +26,9 @@ const DynamicPage = () => {
   const [contributorType, setContributorType] = useState(pageData.contributorType || '');
   const [split, setSplit] = useState<number>(pageData.split || 0);
   const [splitTotal, setSplitTotal] = useState<number>(lastSplit || 0);
-
   const resetPages = useDynamicPageStore((state) => state.resetPages);
+  const {lng} = params
+  const { t } = useTranslation(lng, 'musical_work/dynamic')
 
   useEffect(() => {
     resetPages(pageNumber); // Reset all stored info 
@@ -86,19 +92,19 @@ const DynamicPage = () => {
               onClick={() => router.push('/question1')}
               className="text-xs text-gray-500 w-full border-0 relative text-start border-none"
             >
-              What type of splits contract would you like to create?
+              {t('back1')}
             </button>
             <button
               onClick={() => router.push('/musical_work/question2')}
               className="text-xs text-gray-500 w-full border-0 relative text-start border-none"
             >
-              What is the name of the song?
+              {t('back2')}
             </button>
             <button
               onClick={() => router.push('/musical_work/question3')}
               className="text-xs text-gray-500 w-full border-0 relative text-start border-none"
             >
-              How many collaborators contributed to writing the song?
+              {t('back3')}
             </button>
 
             {Array.from({ length: pageNumber - 1 }, (_, i) => (
@@ -107,14 +113,14 @@ const DynamicPage = () => {
                 onClick={() => router.push(`/musical_work/${i + 1}?pageCount=${pageCount}`)}
                 className="text-xs text-gray-500 w-full border-0 relative text-start border-none"
               >
-                Contributor {i + 1}
+                {t('contributor')} {i + 1}
               </button>
             ))}
           </div>
-          <h2 className="text-lg sm:text-xl mb-4">Contributor {pageNumber}</h2>
+          <h2 className="text-lg sm:text-xl mb-4">{t('contributor')} {pageNumber}</h2>
           <form className="flex flex-col gap-4">
             <div>
-              <label className="text-xs sm:text-sm mb-2 block">Legal Name (First Last)</label>
+              <label className="text-xs sm:text-sm mb-2 block">{t('name')}</label>
               <input
                 type="text"
                 value={legalName}
@@ -124,7 +130,7 @@ const DynamicPage = () => {
               />
             </div>
             <div>
-              <label className="text-xs sm:text-sm mb-2 block">Email (example@mesawallet.io)</label>
+              <label className="text-xs sm:text-sm mb-2 block">{t('email')}</label>
               <input
                 type="email"
                 value={email}
@@ -134,7 +140,7 @@ const DynamicPage = () => {
               />
             </div>
             <div>
-              <label className="text-xs sm:text-sm mb-2 block">Type of contributor</label>
+              <label className="text-xs sm:text-sm mb-2 block">{t("type")}</label>
               <select
                 name="type"
                 id="cont"
@@ -144,13 +150,13 @@ const DynamicPage = () => {
                 required
               >
                 <option value="blank"></option>
-                <option value="Lyrics">Lyrics</option>
-                <option value="Music">Music</option>
-                <option value="Music and Lyrics">Both</option>
+                <option value="Lyrics">{t('lyrics')}</option>
+                <option value="Music">{t('music')}</option>
+                <option value="Music and Lyrics">{t('both')}</option>
               </select>
             </div>
             <div>
-              <label className="text-xs sm:text-sm mb-2 block">Split (%)</label>
+              <label className="text-xs sm:text-sm mb-2 block">{t("split")}</label>
               <input
                 type="number"
                 max="100"
@@ -163,26 +169,24 @@ const DynamicPage = () => {
         </div>
         <div className="w-full sm:w-1/2 p-4 sm:p-8">
           <p className="text-xs sm:text-sm text-gray-500 mb-4">
-            Your contract has yet to be completed. Continue to fill out the decision tree.
+            {t("p1")}
           </p>
-          <h3 className="text-base sm:text-lg font-bold mb-2">1.0 Music Work Identification</h3>
+          <h3 className="text-base sm:text-lg font-bold mb-2"></h3>
           <p className="text-sm sm:text-base mb-4">
-            The parties acknowledge and accept their contribution to the
-            authorship or composition of the musical work and agree to the
-            distribution of copyright ownership as follows:
+            {t("p3")}
           </p>
           <h3 className="text-base sm:text-lg font-bold mb-2">Collaborator {pageNumber}:</h3>
           <p className="text-sm sm:text-base">
-            Legal Name: <span className="text-red-500">{legalName}</span>
+            {t('name2')}: <span className="text-red-500">{legalName}</span>
           </p>
           <p className="text-sm sm:text-base">
-            Email Address: <span className="text-red-500">{email}</span>
+            {t("email2")}: <span className="text-red-500">{email}</span>
           </p>
           <p className="text-sm sm:text-base">
-            Contribution: <span className="text-red-500">{contributorType}</span>
+            {t("contributor2")}: <span className="text-red-500">{contributorType}</span>
           </p>
           <p className="text-sm sm:text-base">
-            Split (%): <span className="text-red-500">{split}</span>
+            {t('split2')}: <span className="text-red-500">{split}</span>
           </p>
         </div>
       </main>
@@ -192,7 +196,7 @@ const DynamicPage = () => {
           onClick={handleNextPage}
           className=" text-white py-2 px-4 rounded-lg w-full"
         >
-          Next
+          {t('next')}
         </button>
       </footer>
     </div>
