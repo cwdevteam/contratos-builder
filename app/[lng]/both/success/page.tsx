d65f101b614eb268c3217cccb28cb64c81d5aa78
Nick Carney <npcarney@uvm.edu>
2024-11-06 18:11:30 -0500

    translation of both/success

--- app/[lng]/both/success/page.tsx
+++ app/[lng]/both/success/page.tsx
@@ -2,18 +2,23 @@
 
 import React from 'react'
 import { useRouter } from 'next/navigation'
-
 import PDF from './pdf'
 import { loadStripe } from '@stripe/stripe-js';
+import { useTranslation } from '@/app/i18n/client'
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-const Success = () => {
+const Success = ({ params }: {
+  params: {
+    lng: string;
+  };
+}) => {
   const router = useRouter()
-
   const downloadUnsigned = PDF()
+  const {lng} = params
+  const { t } = useTranslation(lng, 'master/success')
 
   React.useEffect(() => {
     // Check to see if this is a redirect back from Checkout
@@ -48,35 +53,34 @@ const Success = () => {
               onClick={() => router.push('/question1')}
               className="text-xs sm:text-sm text-gray-500 w-full text-left mb-2 border-none"
             >
-              What type of splits contract would you like to create?
+              {t('back1')}
             </button>
           </div>
           <h1 className="text-xl sm:text-2xl font-bold mb-4 text-center">
-            Congrats! You&apos;re protecting your art.
+          {t('congrats')}
           </h1>
           <h2 className="text-2xl sm:text-3xl font-bold mb-8 text-center">
-            DRAFT CONTRACT CREATED!
+          {t('draft-created')}
           </h2>
           <div className="flex flex-col gap-4">
             <button 
               onClick={downloadUnsigned}
               className=" text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors"
             >
-              DOWNLOAD UNSIGNED VERSION
+              {t('download-unsiged')}
             </button>
             <button 
               onClick={handleCheckout}
               className=" text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors"
             >
-              SEND DOCUSIGN TO COLLABORATORS
+              {t('send-docusign')}
             </button>
           </div>
         </div>
         <div className="w-full sm:w-1/2 p-4 sm:p-8 flex flex-col justify-center">
-          <p className="text-lg sm:text-xl mb-8">Congrats! You&apos;re protecting your art.</p>
+          <p className="text-lg sm:text-xl mb-8">{t('congrats')}</p>
           <h4 className="text-base sm:text-lg">
-            Don&apos;t forget to review it with your colleagues or with a lawyer,
-            customize it if needed and sign it when you are ready!
+          {t('lawyer')}
           </h4>
         </div>
       </main>
