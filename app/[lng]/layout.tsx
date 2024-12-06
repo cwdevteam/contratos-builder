d8bbc74d77014186517ff38c365e677704e8027a
Nick Carney <npcarney@uvm.edu>
2024-12-06 12:08:40 -0500

    using speedInsights

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -44,6 +44,7 @@ export default async function RootLayout({
 
   return (
     <html lang={lng} dir={dir(lng)}>
+    <SpeedInsights/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
