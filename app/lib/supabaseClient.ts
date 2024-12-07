8d1d04af9d239238a5975dfd2951eaeed9a725c1
Nick Carney <npcarney@uvm.edu>
2024-12-07 17:34:44 -0500

    adding value

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ewvzsofyvxcctuxxqibo.supabase.co';
-//const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3dnpzb2Z5dnhjY3R1eHhxaWJvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5Njk0NTMxMywiZXhwIjoyMDEyNTIxMzEzfQ.2soO0wUz1w5uO9640joQmZ-I-AOkhqKxRhrU4N9NK5s';
 
-export const supabase = createClient(supabaseUrl, process.env.SUPABASE_KEY!);
+export const supabase = createClient(supabaseUrl, supabaseKey);
