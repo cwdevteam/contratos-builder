cfe20e21d4ca3826b0b75ba7d337a0d867d36825
Nick Carney <npcarney@uvm.edu>
2024-12-18 13:38:11 -0500

    disclaimer page

--- app/[lng]/popups/disclaimer/page.tsx
+++ app/[lng]/popups/disclaimer/page.tsx
@@ -1,16 +1,18 @@
-'use client';
+"use client";
 
-import React from 'react';
-import {useRouter} from 'next/navigation'
-import { useTranslation } from '@/app/i18n/client';
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useTranslation } from "@/app/i18n/client";
 
-const Disclaimer = ({ params }: {
+const Disclaimer = ({
+  params,
+}: {
   params: {
     lng: string;
   };
 }) => {
   const { lng } = params;
-  const { t } = useTranslation(lng, 'popups/disclaimer');
+  const { t } = useTranslation(lng, "popups/disclaimer");
   const { push } = useRouter();
 
   return (
@@ -18,17 +20,23 @@ const Disclaimer = ({ params }: {
       <main className="flex flex-col gap-6 sm:gap-8">
         <p className="text-sm sm:text-base space-y-4">
           <span className="block">
-            {t('1')}
-            <br /><br />
-            {t('2')}
-            <br /><br />
-            {t('3')}
-            </span>
+            {t("1")}
+            <br />
+            <br />
+            {t("2")}
+            <br />
+            <br />
+            {t("3")}
+          </span>
         </p>
-        <button onClick={() => push('/question1')} className="float-right">{t('4')}</button>
+        <button
+          onClick={() => push("/question1")}
+          className="w-1/5 bg-[#232323]/[.7] border-[#828282]/[.7]"
+        >
+          {t("4")}
+        </button>
       </main>
     </div>
-    
   );
 };
 
