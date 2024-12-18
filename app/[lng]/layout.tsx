917fa7756bff2a58a25be61427dd625780245719
Nick Carney <npcarney@uvm.edu>
2024-12-18 13:15:25 -0500

    removing unused 'localfont'

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Image from "next/image";
 import mesaImage from "./public/images/mesa_logo.png";
