d24834a8f85e46849b234e67123bbeb15bf8ccea
Nick Carney <npcarney@uvm.edu>
2024-12-18 13:12:57 -0500

    figma match landing page

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,34 +1,23 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
-import Image from 'next/image'
-import mesaImage from './public/images/mesa_logo.png'
-import { dir } from 'i18next'
-import { languages } from '../i18n/settings'
-import LanguageSwitcher from '../components/LanguageSwitcher'
-import { useTranslation } from '../i18n'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Image from "next/image";
+import mesaImage from "./public/images/mesa_logo.png";
+import { dir } from "i18next";
+import { languages } from "../i18n/settings";
+import LanguageSwitcher from "../components/LanguageSwitcher";
+import { useTranslation } from "../i18n";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export async function generateStaticParams() {
-  return languages.map((lng) => ({ lng }))
+  return languages.map((lng) => ({ lng }));
 }
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
 
 export const metadata: Metadata = {
-  title: 'Mesa contract builder',
-  description: 'create music splits contracts',
-}
+  title: "Mesa contract builder",
+  description: "create music splits contracts",
+};
 
 export default async function RootLayout({
   children,
@@ -39,40 +28,32 @@ export default async function RootLayout({
     lng: string;
   };
 }) {
-  const { lng } = await params// eslint-disable-line react-hooks/rules-of-hooks
-  const {t} = await useTranslation(lng)// eslint-disable-line react-hooks/rules-of-hooks
-
+  const { lng } = await params; // eslint-disable-line react-hooks/rules-of-hooks
+  const { t } = await useTranslation(lng); // eslint-disable-line react-hooks/rules-of-hooks
 
   return (
     <html lang={lng} dir={dir(lng)}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <header className="flex items-center justify-between h-20 px-4">
           <div className="flex items-center space-x-2">
-          <Image
-            src={mesaImage}
-            width={50}
-            height={50}
-            alt="M"
-          ></Image>
-          <p className="float-left">
-            <b> mesa</b>
-          </p>
+            <Image src={mesaImage} width={190} height={60} alt="M"></Image>
+            <p className="float-left text-[32px]"></p>
           </div>
           <div>
-        <LanguageSwitcher/>
-        </div>
-          <div className="text-right text-xs">
-            <p className="text-2xl font-black">MUSIC SPLITS</p>
-            {t('contract-builder')}
+            <LanguageSwitcher />
+          </div>
+          <div className="text-right text-xs w-[17rem]">
+            <p className="text-2xl font-black font-rubik">MUSIC SPLITS</p>
+            <div className="font-share text-center">
+              {t("contract-builder")}
+            </div>
           </div>
         </header>
         <hr className="w-full absolute" />
         {children}
-        <SpeedInsights/>
-        <Analytics/>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { languages, fallbackLng } from '../i18n/settings'
-import { useTranslation } from '../i18n/client'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { languages, fallbackLng } from "../i18n/settings";
+import { useTranslation } from "../i18n/client";
 
 interface PopupProps {
-  onClose: () => void
+  onClose: () => void;
   params: {
     lng: string;
   };
@@ -17,39 +17,38 @@ const Popup = ({ onClose, params }: PopupProps) => {
   const { t } = useTranslation(lng);
   return (
     <div className="popup flex flex-col p-4 sm:p-8 text-sm sm:text-base">
-      <p>
-        {t('popups.1')}
-      </p>
+      <p>{t("popups.1")}</p>
       <ul>
         <li>
-        {t('popups.2')}
+          {t("popups.2")}
           <br />
-          {t('popups.3')}
+          {t("popups.3")}
           <br />
-          {t('popups.4')}
-        </li>
-        <li>
-        {t('popups.5')}
-          <br />
-          {t('popups.6')}
-          <br />
-          {t('popups.7')}
+          {t("popups.4")}
         </li>
         <br />
         <li>
-        {t('popups.8')}
+          {t("popups.5")}
           <br />
-          {t('popups.9')}
+          {t("popups.6")}
           <br />
-          {t('popups.10')}
+          {t("popups.7")}
+        </li>
+        <br />
+        <li>
+          {t("popups.8")}
+          <br />
+          <ol className="list-disc pl-5">
+            <li>{t("popups.9")}</li>
+            <br />
+            <li>{t("popups.10")}</li>
+          </ol>
           <br />
         </li>
       </ul>
-      {t('popups.11')}
+      {t("popups.11")}
       <br />
-      <p>
-      {t('popups.12')}
-      </p>
+      <p>{t("popups.12")}</p>
       <button
         onClick={onClose}
         className="popup_button text-white hover:text-gray-300"
@@ -57,71 +56,69 @@ const Popup = ({ onClose, params }: PopupProps) => {
         &times;
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default function Home({ params }: {
+export default function Home({
+  params,
+}: {
   params: {
     lng: string;
   };
 }) {
-  let { lng } =  params
-  if (languages.indexOf(lng) < 0) lng = fallbackLng
+  let { lng } = params;
+  if (languages.indexOf(lng) < 0) lng = fallbackLng;
 
-
-  const { push } = useRouter()
-  const [showPopup, setShowPopup] = useState(false)
+  const { push } = useRouter();
+  const [showPopup, setShowPopup] = useState(false);
 
   const togglePopup = () => {
-    setShowPopup(!showPopup)
-  }
+    setShowPopup(!showPopup);
+  };
 
-  const { t } = useTranslation(lng)
+  const { t } = useTranslation(lng);
 
   useEffect(() => {
-    if(lng=="es"){
-      document.getElementById('li4')!.innerHTML="<li>"+t('li4')+"</li>"
+    if (lng == "es") {
+      document.getElementById("li4")!.innerHTML = "<li>" + t("li4") + "</li>";
     }
   }, []);
 
-  
-
   return (
-    <div className="p-4 sm:p-8 flex flex-col justify-between">
-      <main className="flex flex-col gap-6 sm:gap-8">
-        <h3 className="text-xl sm:text-2xl font-bold">
-          <b>{t('h3')}</b>
-         
-        </h3>
-        <p className="text-sm sm:text-base">
-          {t('intro')}
+    <div className="flex flex-col items-center md:items-start pl-10 pt-20  p-2 w-3/5">
+      <section className="w-full flex flex-col gap-5">
+        <p className="md:text-[32px] text-[20px] font-share tracking-[-0.05rem] text-start">
+          {t("h3-1")}
+          <span className="font-rubik">{t("mesa")}</span>
+          {t("h3-2")}
         </p>
-        <p className="text-sm sm:text-base">
-          {t('li-title')}
+        <p className="md:text-[24px] text-2xl font-share tracking-[-0.05rem] text-start">
+          {t("intro")}
+          <br />
+          {t("li-title")}
         </p>
-        <ol className="list-decimal pl-6 text-sm sm:text-base">
-          <li>{t('li1')}</li>
-          <li>{t('li2')}</li>
-          <li>{t('li3')}</li>
-          <span id='li4'></span>
+        <ol className="list-disc pl-6 text-sm sm:text-base text-start md:text-[24px] text-[18px] font-share tracking-[-0.05rem] pt-4">
+          <li className="pb-2">{t("li1")}</li>
+          <li className="pb-2"> {t("li2")}</li>
+          <li className="pb-2">{t("li3")}</li>
+          <span id="li4"></span>
         </ol>
-      </main>
-      <footer className="mt-8 flex flex-col gap-4">
+      </section>
+      <footer className="mt-8 flex flex-col gap-4 text-start pt-4">
         <a
-          className="text-blue-500 hover:underline hover:underline-offset-4 text-sm sm:text-base"
+          className="font-share text-2xl text-link max-w-fit underline text-start"
           href="#"
           onClick={togglePopup}
         >
-          {t('confused')}
+          {t("confused")}
         </a>
-        <button
-          onClick={() => push(`/${lng}/popups/disclaimer`)}
-          className="text-white py-2 px-4 rounded  transition-colors w-1/4"
-        >
-          {t('get-started')}
+        <button onClick={() => push(`/${lng}/popups/disclaimer`)}>
+          {t("get-started")}
         </button>
-        {showPopup && <Popup onClose={() => setShowPopup(false)} params={{lng:lng}} />}
+        {showPopup && (
+          <Popup onClose={() => setShowPopup(false)} params={{ lng: lng }} />
+        )}
       </footer>
     </div>
-  )
+  );
 }
