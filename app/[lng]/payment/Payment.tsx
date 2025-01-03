9416bd5c1cbc87f33755f32edb140ab365e784fb
Nick Carney <npcarney@uvm.edu>
2025-01-03 12:55:48 -0500

    MES-32 pt1

--- app/[lng]/payment/Payment.tsx
+++ app/[lng]/payment/Payment.tsx
@@ -1,22 +1,37 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import useDynamicPageStore from '../store/use[page]';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import useDynamicPageStore from "../store/use[page]";
+import { useTranslation } from "@/app/i18n/client";
 
-const Payment = () => {
+const Payment = ({
+  params,
+}: {
+  params: {
+    lng: string;
+  };
+}) => {
+  const { lng } = params;
+  const { t } = useTranslation(lng, "confirmation");
   const query = useSearchParams();
-  const paid = query.get('success');
+  const paid = query.get("success");
   const pages = useDynamicPageStore((state) => state.pages);
-  const emails = Object.keys(pages).map((id) => pages[Number(id)].email);
+  console.log("pages:", pages);
+  const emails = Object.keys(pages).map((id) => {
+    const email = pages[Number(id)]?.email;
+    console.log(`id: ${id}, email: ${email}`);
+    return email;
+  });
+  console.log("emails:", emails);
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    if (paid === 'true') {
-      setMessage(`A contract will be sent to ${emails.join(', ')}`);
+    if (paid === "true") {
+      setMessage(t("1") + emails.join(", "));
     } else {
-      setMessage('Your payment was not successful');
+      setMessage(t("2"));
     }
   }, [paid, emails]);
 
@@ -24,10 +39,8 @@ const Payment = () => {
     <div className=" p-4 sm:p-8 flex flex-col justify-between">
       <main>
         <div className="w-full sm:w-1/2 p-4 sm:p-8 flex flex-col justify-center">
-          <p className="text-lg sm:text-xl mb-8">Congrats! You’re protecting your art.</p>
-          <div className="text-base sm:text-lg">
-            {message}
-          </div>
+          <p className="text-lg sm:text-xl mb-8">{t("3")}</p>
+          <div className="text-base sm:text-lg">{message}</div>
         </div>
       </main>
     </div>
