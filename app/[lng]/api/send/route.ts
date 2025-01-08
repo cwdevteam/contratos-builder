c16b1f3b03fa886362bed4e6faf2b4d49dd75998
Nick Carney <npcarney@uvm.edu>
2025-01-07 19:25:35 -0500

    resend work

--- app/[lng]/api/send/route.ts
+++ app/[lng]/api/send/route.ts
@@ -1,27 +1,55 @@
+// import { EmailTemplate } from "../../../components/email-template";
+// import { Resend } from "resend";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function POST() {
+//   try {
+
+//     const { data, error } = await resend.emails.send({
+//       from: "Mesa <contracts@mesawallet.io>",
+//       to: [
+//         "nick@mesawallet.io",
+//         //"andres@mesawallet.io",
+//         //"thomas@mesawallet.io",
+//       ],
+//       subject: "Contract Payment Successful",
+//       react: EmailTemplate({ songName:  }),
+//     });
+
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
+
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
+
+import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "../../../components/email-template";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "Mesa <contracts@mesawallet.io>",
-      to: [
-        "nick@mesawallet.io",
-        //"andres@mesawallet.io",
-        //"thomas@mesawallet.io",
-      ],
-      subject: "Contract Payment Successful",
-      react: EmailTemplate({ songName: "song Name" }),
-    });
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { songName } = req.body; // Assuming the name is sent in the request body
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+  const { data, error } = await resend.emails.send({
+    from: "Mesa <contracts@mesawallet.io>",
+    to: [
+      "nick@mesawallet.io",
+      //"andres@mesawallet.io",
+      //"thomas@mesawallet.io",
+    ],
+    subject: "Contract Download Successful",
+    react: EmailTemplate({ songName }),
+  });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
+  if (error) {
+    return res.status(400).json(error);
   }
-}
+
+  res.status(200).json(data);
+};
--- app/components/email-template.tsx
+++ app/components/email-template.tsx
@@ -8,6 +8,6 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   songName,
 }) => (
   <div>
-    <h1>A contract has been created for the song: {songName}!</h1>
+    <p>A contract has been created for the song: {songName}!</p>
   </div>
 );
