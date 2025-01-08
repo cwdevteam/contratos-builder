e8573360605082575abed9efe9b3a922bfcf0620
Nick Carney <npcarney@uvm.edu>
2025-01-07 19:29:36 -0500

    this is the one

--- app/[lng]/api/send/route.ts
+++ app/[lng]/api/send/route.ts
@@ -27,29 +27,32 @@
 //   }
 // }
 
-import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "../../../components/email-template";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { songName } = req.body; // Assuming the name is sent in the request body
+export async function POST(request: Request) {
+  const { songName } = await request.json(); // Assuming the name is sent in the request body
 
-  const { data, error } = await resend.emails.send({
-    from: "Mesa <contracts@mesawallet.io>",
-    to: [
-      "nick@mesawallet.io",
-      //"andres@mesawallet.io",
-      //"thomas@mesawallet.io",
-    ],
-    subject: "Contract Download Successful",
-    react: EmailTemplate({ songName }),
-  });
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Mesa <contracts@mesawallet.io>",
+      to: [
+        "nick@mesawallet.io",
+        //"andres@mesawallet.io",
+        //"thomas@mesawallet.io",
+      ],
+      subject: "Contract Payment Successful",
+      react: EmailTemplate({ songName }),
+    });
 
-  if (error) {
-    return res.status(400).json(error);
+    if (error) {
+      return Response.json({ error }, { status: 400 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
   }
-
-  res.status(200).json(data);
-};
+}
