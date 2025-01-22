a4da715a43c322b5b46e723d42adc953aa66d115
Nick Carney <npcarney@uvm.edu>
2025-01-22 12:47:18 -0500

    master_path update

--- app/[lng]/master_recording/question4/page.tsx
+++ app/[lng]/master_recording/question4/page.tsx
@@ -1,120 +1,50 @@
-'use client'
+"use client";
 
-import React, { Suspense, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { useSearchParams } from 'next/navigation'
-import useQuestion4 from '../../store/useQuestion4'
-import { useTranslation } from '@/app/i18n/client'
+import React, { Suspense, useState } from "react";
+import { useRouter } from "next/navigation";
+import useQuestion4 from "../../store/useQuestion4";
+import { useTranslation } from "@/app/i18n/client";
+import Popup from "reactjs-popup";
 
-interface PopupProps {
-  onClose: () => void
-  params: {
-    lng: string;
-  };
-}
-
-const Popup = ({ onClose, params }: PopupProps) => {
-  const {push} = useRouter()
-  const {lng} = params
-  const {t} = useTranslation(lng, 'master/question4');
-  return (
-    <div className="popup flex-col">
-      <p>
-        {t('popups.1')}
-      </p>
-      <a
-        className="items-center gap-2 hover:underline hover:underline-offset-4"
-        onClick={() => push('/popups/moreInfoVoting')}
-      >
-        {t('popups.2')}
-      </a>
-      <a
-        className="items-center gap-2 hover:underline hover:underline-offset-4"
-        onClick={() => push('/popups/moreInfoAdmin')}
-      >
-        {t('popups.3')}
-      </a>
-      <button onClick={onClose} className="popup_button">
-        x
-      </button>
-    </div>
-  )
-}
-
-const ContractBuilder4 = ({ params }: {
+const ContractBuilder4 = ({
+  params,
+}: {
   params: {
     lng: string;
   };
 }) => {
-  const {push} = useRouter()
+  const { push } = useRouter();
 
-  const [showPopup, setShowPopup] = useState(false)
-  const searchParams = useSearchParams()
-  const pageCount = Number(searchParams.get('pageCount'))
-  const [selectedOption, setSelectedOption] = useState('')
-  const updateVoteSelection = useQuestion4((state) => state.updateVoteSelection);
-  const {lng} = params
-  const {t} = useTranslation(lng, 'master/question4');
-
-  const goToPage = (page: number) => {
-    push(`/master_recording/${page}?pageCount=${pageCount}`)
-  }
-
-  const togglePopup = () => {
-    setShowPopup(!showPopup)
-  }
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const updateVoteSelection = useQuestion4(
+    (state) => state.updateVoteSelection
+  );
+  const { lng } = params;
+  const { t } = useTranslation(lng, "master/question4");
 
   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(event.target.value)
-  }
+    setSelectedOption(event.target.value);
+  };
 
   const findNextPage = () => {
-    updateVoteSelection(selectedOption)
-    if (selectedOption == 'VOTE') {
-      push('/master_recording/question5vote')
-    } else if (selectedOption == "ADMIN"){
-      push('/master_recording/question5admin')
+    updateVoteSelection(selectedOption);
+    if (selectedOption == "VOTE") {
+      push("/master_recording/question5vote");
+    } else if (selectedOption == "ADMIN") {
+      push("/master_recording/question5admin");
     }
-  }
+  };
 
   return (
     <div className=" p-4 sm:p-8 flex flex-col justify-between">
       <main className="flex flex-col sm:flex-row gap-6 sm:gap-8">
         <div className="w-full sm:w-1/2 py-4 sm:py-10">
-          <button
-            onClick={() => push('/question1')}
-            className="text-xs sm:text-sm text-gray-500 w-full text-left mb-2 border-none p-0"
-          >
-            {t('back1')}
-          </button>
-          <button
-            onClick={() => push('/master_recording/question2')}
-            className="text-xs sm:text-sm text-gray-500 w-full text-left mb-4 border-none p-0"
-          >
-            {t('back2')}
-          </button>
-          <button
-            onClick={() => push('/master_recording/question3')}
-            className="text-xs sm:text-sm text-gray-500 w-full text-left mb-4 border-none p-0"
-          >
-            {t('back3')}
-          </button>
-
-          {Array.from({ length: pageCount }, (_, i) => (
-            <button
-              key={i + 1}
-              onClick={() => goToPage(i + 1)}
-              className="text-xs sm:text-sm text-gray-500 w-full text-left mb-2 border-none p-0"
-            >
-              {t('contributor')} {i + 1}
-            </button>
-          ))}
-
-          <h4 className="text-sm sm:text-base mb-4 pt-5">
-          {t('question')}
+          <h4 className="mb-4 pt-5 font-share text-[1.5rem]">
+            {t("question")}
           </h4>
           <form className="flex flex-col gap-2">
-            <label className="flex items-center">
+            <label className="flex items-center font-rubik">
               <input
                 type="radio"
                 name="type"
@@ -123,9 +53,9 @@ const ContractBuilder4 = ({ params }: {
                 onChange={handleRadioChange}
                 required
               />
-              <span className="text-sm sm:text-base">{t('vote')}</span>
+              <span className="text-sm sm:text-base">{t("vote")}</span>
             </label>
-            <label className="flex items-center">
+            <label className="flex items-center font-rubik">
               <input
                 type="radio"
                 name="type"
@@ -134,46 +64,89 @@ const ContractBuilder4 = ({ params }: {
                 onChange={handleRadioChange}
                 required
               />
-              <span className="text-sm sm:text-base">{t('admin')}</span>
+              <span className="text-sm sm:text-base">{t("admin")}</span>
             </label>
           </form>
         </div>
         <div className="w-full sm:w-1/2 p-4 sm:p-8">
-          <p className="text-xs sm:text-sm text-gray-500 mb-4">
-          {t('p1')}
-          </p>
+          <p className="text-gray-500 mb-4 font-share pt-20">{t("p1")}</p>
         </div>
       </main>
       <footer className="mt-8 flex flex-col gap-4">
-        <a
-          className="text-blue-500 hover:underline hover:underline-offset-4 text-sm sm:text-base"
-          href="#"
-          onClick={togglePopup}
-        >
-          {t('confused')}
-        </a>
-        <button 
-          onClick={findNextPage}
-          className="text-white py-2 px-4 rounded  transition-colors w-1/5"
-        >
-          {t('submit')}
-        </button>
-        {showPopup && <Popup onClose={() => setShowPopup(false)} params={{lng:lng}}/>}
+        {!isOpen && (
+          <Popup
+            trigger={
+              <a className="text-[#3167B4] underline underline-offset-4 text-sm sm:text-base ">
+                {t("confused")}
+              </a>
+            }
+            position="right center"
+            modal
+            nested
+            className="popup"
+            closeOnDocumentClick
+          >
+            <div
+              className="modal border-2 border-white"
+              style={{ height: "60vh", overflowY: "scroll" }}
+            >
+              <p>{t("popups.1")}</p>
+              <a
+                className="items-center gap-2 hover:underline hover:underline-offset-4"
+                onClick={() => push("/popups/moreInfoVoting")}
+              >
+                {t("popups.2")}
+              </a>
+              <a
+                className="items-center gap-2 hover:underline hover:underline-offset-4"
+                onClick={() => push("/popups/moreInfoAdmin")}
+              >
+                {t("popups.3")}
+              </a>
+            </div>
+            <button
+              onClick={() => {
+                setIsOpen(true);
+                setTimeout(() => {
+                  setIsOpen(false);
+                }, 200);
+              }}
+              className="popup_button text-white hover:text-gray-300"
+            >
+              &times;
+            </button>
+          </Popup>
+        )}
+
+        <div className="inline-flex gap-20">
+          <button
+            onClick={() => push("/master_recording/question3")}
+            className=" w-fit bg-[#AC444475]"
+          >
+            {t("back")}
+          </button>
+          <button onClick={findNextPage} className=" w-fit bg-[#AC444475]">
+            {t("submit")}
+          </button>
+        </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-const WrappedContractBuilder4 = ({ params }: {
+const WrappedContractBuilder4 = ({
+  params,
+}: {
   params: {
     lng: string;
   };
 }) => {
-  const {lng} = params
-  return(
-  <Suspense fallback={<div>Loading...</div>}>
-    <ContractBuilder4 params={{lng:lng}}/>
-  </Suspense>
-)}
+  const { lng } = params;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ContractBuilder4 params={{ lng: lng }} />
+    </Suspense>
+  );
+};
 
-export default WrappedContractBuilder4
+export default WrappedContractBuilder4;
