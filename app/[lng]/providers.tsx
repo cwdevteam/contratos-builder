f059a2566c82e2f6c498c2201504494056a160c9
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-01-31 23:02:39 -0500

    style/brand crypto payment button

--- app/[lng]/providers.tsx
+++ app/[lng]/providers.tsx
@@ -2,14 +2,20 @@
  
 import type { ReactNode } from 'react';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { base } from 'wagmi/chains'; // add baseSepolia for testing
+import { base } from 'wagmi/chains';
  
 export function Providers(props: { children: ReactNode }) {
   return (
     <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base} // add baseSepolia for testing
-    >
+      chain={base}
+      config={{
+        appearance: {
+          name: 'Mesa Wallet', 
+          logo: 'https://mesa.mypinata.cloud/ipfs/bafkreifesm2kzzlgzamioix4i5yt3mwvpxqh5vaq6k4b5lzmbk4yuaaiwu', 
+        },
+      }}
+      >
       {props.children}
     </OnchainKitProvider>
   );
