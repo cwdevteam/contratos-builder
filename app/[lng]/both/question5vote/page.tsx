64c6edf687006be11d5c5d09f39b44617949c73b
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-02-11 16:42:47 -0500

    more descriptive vars both/question5vote

--- app/[lng]/both/question5vote/page.tsx
+++ app/[lng]/both/question5vote/page.tsx
@@ -35,7 +35,7 @@ const ContractBuilder5Vote = ({
       <main className="flex flex-col sm:flex-row gap-6 sm:gap-8">
         <div className="w-full sm:w-1/2 sm:py-10">
           <h1 className="text-lg sm:text-xl mb-4 font-share w-full">
-            {t("percent")}
+            {t("ownershipPercentageQuestion")}
           </h1>
           <form className="flex flex-col">
             <label className="text-xs sm:text-sm mb-2">(%)</label>
@@ -48,23 +48,23 @@ const ContractBuilder5Vote = ({
         </div>
         <div className="w-full sm:w-1/2 sm:p-8">
           <p className="text-gray-500 mb-4 font-roboto_light text-[0px] sm:text-[16px]">
-            {t("p1")}
+            {t("incompleteContractMessage")}
           </p>
           <h4 className="text-base sm:text-lg font-bold mb-2 font-roboto_bold">
-            {t("2.0")}
+            {t("rightsAndDutiesTitle")}
           </h4>
           <p className="text-xs sm:text-sm mb-4 font-roboto_thin">
-            {t("p2")}{" "}
+            {t("legalActionsRestriction")}{" "}
             <span className="text-[#AC4444] font-rubik">
               {percent ? percent : " "}%
             </span>
-            {t("p3")}
+            {t("ownershipDetails")}
           </p>
           <ol className="list-decimal pl-5 text-xs sm:text-sm">
-            <li>{t("li1")}</li>
-            <li>{t("li2")}</li>
-            <li>{t("li3")}</li>
-            <li>{t("li4")}</li>
+            <li>{t("licenseAuthorization")}</li>
+            <li>{t("editPermission")}</li>
+            <li>{t("nameExploitationRestriction")}</li>
+            <li>{t("grantLicenses")}</li>
           </ol>
         </div>
       </main>
@@ -74,20 +74,20 @@ const ContractBuilder5Vote = ({
           href="#"
           onClick={() => push("/popups/moreInfoVoting")}
         >
-          {t("confused")}
+          {t("confusedMessage")}
         </a>
         <div className="inline-flex relative bottom-0 left-0 right-0 justify-between sm:justify-normal sm:gap-20 gap-5 sm:mt-[3%]">
           <button
             onClick={() => push("/both/question4")}
             className="  w-[15%]  bg-[#AC444475] flex-1 sm:flex-none "
           >
-            {t("back")}
+            {t("backButton")}
           </button>
           <button
             onClick={handleSubmit}
             className="  w-[15%]  bg-[#AC444475] flex-1 sm:flex-none "
           >
-            {t("submit")}
+            {t("submitButton")}
           </button>
         </div>
       </footer>
