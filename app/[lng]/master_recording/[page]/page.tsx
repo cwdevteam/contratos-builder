98ce6df7878559b2d68ef6eea218c3d4cddd4069
Nick Carney <nickcarney@Nicks-Air.lan>
2025-02-19 14:46:12 -0500

    fixing split % check colab page - tiny

--- app/[lng]/master_recording/[page]/page.tsx
+++ app/[lng]/master_recording/[page]/page.tsx
@@ -119,7 +119,7 @@ const DynamicPage = ({
   };
 
   const handleNextPage = () => {
-    if (pageNumber >= pageCount && splitTotal !== 100) {
+    if (pageNumber >= pageCount && splitTotal != 100) {
       const splitNeeded = (100 - splitTotal + split).toFixed(2);
       document.getElementById("wrongSplits")!.innerHTML =
         t("splitHelp") + ". You need " +
