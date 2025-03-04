03cf16d13b47ace6a51ee5afd5bf6c19a8f60444
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-03-04 15:32:52 -0500

    also comment out agent import

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -8,7 +8,7 @@ import { languages } from "../i18n/settings";
 import { useTranslation } from "../i18n";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import Agent from "../components/agent";
+//import Agent from "../components/agent";
 
 import { Providers } from './providers'; 
 
